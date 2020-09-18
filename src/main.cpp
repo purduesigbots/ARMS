634399660e84e83b4662624aaad8f895ddf3aea1
Mihir Laud <mihirlaud@gmail.com>
2020-09-17 20:11:56 -0400

    Output IMU rotation to terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "main.h"
+#include "pros/imu.hpp"
 
 pros::Controller master(CONTROLLER_MASTER);
+pros::Imu imu(12);
 
 void initialize() {
+    imu.reset();
 	initDrive();
 }
 
@@ -16,9 +19,12 @@ void autonomous() {
 }
 
 void opcontrol() {
+
 	while (true) {
 		if (master.get_digital(DIGITAL_LEFT) && !competition::is_connected())
 			autonomous();
+        
+        printf("%f\n", imu.get_rotation());
 
 		arcade(master.get_analog(ANALOG_LEFT_Y) * (double)100 / 127,
 		       master.get_analog(ANALOG_RIGHT_X) * (double)100 / 127);
