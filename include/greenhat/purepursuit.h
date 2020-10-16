ceb9983ef14c6464bc2215e2fb45d11daca36483
Dominic Holifield <dholifi@purdue.edu>
2020-10-15 21:23:32 -0400

    Go To Point function

--- include/greenhat/purepursuit.h
+++ include/greenhat/purepursuit.h
@@ -8,6 +8,10 @@ namespace purepursuit {
 
 std::array<double,2> findIntersectionPoint(std::vector<std::array<double, 2>> path, double radius);
 
+double getAngle(std::array<double, 2> point);
+double getDistance(std::array<double, 2> point);
+
+void goToPoint(std::vector<std::array<double, 2>> path);
 void followPath(std::vector<std::array<double, 2>> path);
 
 } // namespace purepursuit
--- src/greenhat/purepursuit.cpp
+++ src/greenhat/purepursuit.cpp
@@ -6,21 +6,31 @@ using namespace pros;
 namespace purepursuit {
 
 double getAngle(std::array<double, 2> point) {
-    double x = point[0];
-    double y = point[1];
-    
-    x -= greenhat::global_x;
-    y -= greenhat::global_y;
-    double theta = atan2(y,x);
+  double x = point[0];
+  double y = point[1];
 
-    double delta_angle = theta - greenhat::heading;
-    while(fabs(delta_angle) > M_PI) {
-        theta -= (delta_angle / fabs(delta_angle)) * 2 * M_PI;
-        delta_angle = theta - greenhat::heading;
-    }
+  x -= greenhat::global_x;
+  y -= greenhat::global_y;
+  double theta = atan2(y,x);
 
-    return theta;
+  double delta_angle = theta - greenhat::heading;
+  while(fabs(delta_angle) > M_PI) {
+      theta -= (delta_angle / fabs(delta_angle)) * 2 * M_PI;
+      delta_angle = theta - greenhat::heading;
+  }
+  return theta;
 }
+
+
+double getDistance(std::array<double, 2> point) {
+  double x = point[0];
+  double y = point[1];
+
+  x -= greenhat::global_x;
+  y -= greenhat::global_y;
+  return sqrt(x*x + y*y);
+}
+
 
 double lastSpeed = 0;
 int slew(double speed) {
@@ -44,16 +54,6 @@ int slew(double speed) {
 	return lastSpeed;
 }
 
-bool checkForLastSegment(std::vector<std::array<double, 2>> path, std::array<double, 2> point) {
-    std::array<double, 2> last = path.at(path.size() - 1);
-    std::array<double, 2> second_last = path.at(path.size() - 2);
-
-    if(last[0] == second_last[0]) {
-        return last[0] == point[0];
-    } else {
-        if(last[0] > second_last[0])
-    }
-}
 
 bool last_segment = false;
 double m,a,b,c;
@@ -120,7 +120,6 @@ std::array<double,2> findIntersectionPoint(std::vector<std::array<double, 2>> pa
     radius += 1;  // 1 inch, might need to be adjusted
   }
 
-
   last_point = intersection_points.at(0);
 
   // if robot only intersects once on latest segment
@@ -168,9 +167,73 @@ std::array<double,2> findIntersectionPoint(std::vector<std::array<double, 2>> pa
   return target_point;
 }
 
+
 void goToPoint(std::array<double, 2> point) {
-    
+  double kP_vel = 0.0;
+  double kI_vel = 0.0;
+  double kD_vel = 0.0;
+
+  double kP_ang = 0.0;
+  double kI_ang = 0.0;
+  double kD_ang = 0.0;
+
+  double vel_prev_error = getDistance(point);
+  double ang_prev_error = getAngle(point);
+
+  double max_speed = 80;
+  double exit_error = 1;
+
+  while(1) {
+    delay(20);
+
+    double vel_error = getDistance(point);
+    double ang_error = getAngle(point);
+
+    double vel_derivative = vel_error - vel_prev_error;
+    double ang_derivative = ang_error - ang_prev_error;
+
+    vel_prev_error = vel_error;
+    ang_prev_error = ang_error;
+
+    double forward_speed = kP_vel * vel_error + kD_vel * vel_derivative;
+    double turn_modifier = kP_ang * ang_error + kD_ang * ang_derivative;
+
+    double left_speed = forward_speed + turn_modifier;
+    double right_speed = forward_speed - turn_modifier;
+
+    if(left_speed > max_speed) {
+      double diff = left_speed - max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    } else if(left_speed < -max_speed) {
+      double diff = left_speed + max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    }
+
+    if(right_speed > max_speed) {
+      double diff = right_speed - max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    } else if(right_speed < -max_speed) {
+      double diff = right_speed + max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    }
+
+    left_speed = slew(left_speed);
+    right_speed = slew(right_speed);
+
+    greenhat::leftMotors->moveVoltage(left_speed * 120);
+    greenhat::rightMotors->moveVoltage(right_speed * 120);
+
+    if (vel_error < exit_error)
+      break;
+  }
+  greenhat::leftMotors->moveVoltage(0);
+  greenhat::rightMotors->moveVoltage(0);
 }
+
 
 void followPath(std::vector<std::array<double, 2>> path) {
     double inner_radius = 10.0;
@@ -192,58 +255,58 @@ void followPath(std::vector<std::array<double, 2>> path) {
 	while (1) {
 		delay(20);
 
-        std::array<double, 2> ang_tracking_point = findIntersectionPoint(path, inner_radius);
-        std::array<double, 2> vel_tracking_point = findIntersectionPoint(path, outer_radius);
+    std::array<double, 2> ang_tracking_point = findIntersectionPoint(path, inner_radius);
+    std::array<double, 2> vel_tracking_point = findIntersectionPoint(path, outer_radius);
 
-        if(last_segment) {
-            goToPoint(path[path.size() - 1]);
-            break;
-        }
+    if(last_segment) {
+      goToPoint(path[path.size() - 1]);
+      break;
+    }
 
-        double ang_error = getAngle(ang_tracking_point);
-        double vel_error = getAngle(vel_tracking_point);
+    double ang_error = getAngle(ang_tracking_point);
+    double vel_error = getAngle(vel_tracking_point);
 
 		double ang_derivative = ang_error - ang_prev_error;
-        double vel_derivative = vel_error - vel_prev_error;
+    double vel_derivative = vel_error - vel_prev_error;
 		ang_prev_error = ang_error;
-        vel_prev_error = vel_error;
+    vel_prev_error = vel_error;
 
 		double forward_speed;
-        if(vel_error == 0) {
-            forward_speed = max_speed;
-        } else {
-            forward_speed = kP_outer / vel_error - kD_outer * vel_derivative;
-        }
-        double turn_modifier = kP_inner * ang_error + kD_inner * ang_derivative;
+    if(vel_error == 0) {
+      forward_speed = max_speed;
+    } else {
+      forward_speed = kP_outer / vel_error - kD_outer * vel_derivative;
+    }
+    double turn_modifier = kP_inner * ang_error + kD_inner * ang_derivative;
 
-        double left_speed = forward_speed + turn_modifier;
-        double right_speed = forward_speed - turn_modifier;
+    double left_speed = forward_speed + turn_modifier;
+    double right_speed = forward_speed - turn_modifier;
 
-        left_speed = slew(left_speed);
-        right_speed = slew(right_speed);
+    if(left_speed > max_speed) {
+      double diff = left_speed - max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    } else if(left_speed < -max_speed) {
+      double diff = left_speed + max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    }
 
-        if(left_speed > max_speed) {
-            double diff = left_speed - max_speed;
-            left_speed -= diff;
-            right_speed -= diff;
-        } else if(left_speed < -max_speed) {
-            double diff = left_speed + max_speed;
-            left_speed -= diff;
-            right_speed -= diff;
-        }
+    if(right_speed > max_speed) {
+      double diff = right_speed - max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    } else if(right_speed < -max_speed) {
+      double diff = right_speed + max_speed;
+      left_speed -= diff;
+      right_speed -= diff;
+    }
 
-        if(right_speed > max_speed) {
-            double diff = right_speed - max_speed;
-            left_speed -= diff;
-            right_speed -= diff;
-        } else if(right_speed < -max_speed) {
-            double diff = right_speed + max_speed;
-            left_speed -= diff;
-            right_speed -= diff;
-        }
+    left_speed = slew(left_speed);
+    right_speed = slew(right_speed);
 
-        greenhat::leftMotors->moveVoltage(left_speed * 120);
-        greenhat::rightMotors->moveVoltage(right_speed * 120);
+    greenhat::leftMotors->moveVoltage(left_speed * 120);
+    greenhat::rightMotors->moveVoltage(right_speed * 120);
 
 	}
 }
