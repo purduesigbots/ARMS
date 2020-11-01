b628f791bd48d8af82c31b00f2ec26edb18cbb16
micahrassi <mrassi@purdue.edu>
2020-11-01 16:01:06 -0500

    Remove holonomic test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "main.h"
 #include "pros/imu.hpp"
-#include "pros/rtos.hpp"
 
 pros::Controller master(CONTROLLER_MASTER);
 
@@ -16,19 +15,17 @@ void competition_initialize() {
 }
 
 void autonomous() {
-    chassis::moveHolo(1, 45);
 }
 
 void opcontrol() {
 
 	while (true) {
 		if (master.get_digital(DIGITAL_LEFT) && !competition::is_connected()) {
-            autonomous();
-        }
+			autonomous();
+		}
 
-		chassis::holonomic(master.get_analog(ANALOG_LEFT_Y) * (double)100 / 127,
-		                master.get_analog(ANALOG_RIGHT_X) * (double)100 / 127,
-                        master.get_analog(ANALOG_LEFT_X) * (double)100 / 127);
+		chassis::arcade(master.get_analog(ANALOG_LEFT_Y) * (double)100 / 127,
+		                master.get_analog(ANALOG_RIGHT_X) * (double)100 / 127);
 
 		pros::delay(20);
 	}
