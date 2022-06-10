d56f4a5be1c54a4ebc854c78ddb547991bf674c6
Benjamin Davis <davi1561@purdue.edu>
2022-06-10 12:00:15 -0500

    Revert "Added Doxyfile and built"
    This reverts commit c59fef0822e6f424e9a40b7b23a8b9537a4f4462.

--- include/ARMS/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['moveflags_0',['MoveFlags',['../structarms_1_1_move_flags.html',1,'arms']]]
-];
--- include/ARMS/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['point_0',['Point',['../unionarms_1_1_point.html',1,'arms']]]
-];
