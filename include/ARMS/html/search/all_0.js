c59fef0822e6f424e9a40b7b23a8b9537a4f4462
Benjamin Davis <davi1561@purdue.edu>
2022-06-10 12:00:15 -0500

    Added Doxyfile and built

---
+++ include/ARMS/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['moveflags_0',['MoveFlags',['../structarms_1_1_move_flags.html',1,'arms']]]
+];
---
+++ include/ARMS/html/search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['point_0',['Point',['../unionarms_1_1_point.html',1,'arms']]]
+];
