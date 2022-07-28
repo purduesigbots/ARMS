e0d97a52b09bcdc3a18282bd1caf3417314b4e08
Benjamin Davis <davi1561@purdue.edu>
2022-07-28 14:06:48 -0500

    Finish documentation of all the constants in config.h

--- include/ARMS/config.h
+++ include/ARMS/config.h
@@ -4,6 +4,7 @@
 /// @cond DO_NOT_DOCUMENT
 #include "ARMS/lib.h"
 #include "okapi/api.hpp"
+#include "pid.h"
 /// @endcond
 
 namespace arms {
@@ -50,7 +51,7 @@ namespace arms {
  * 
  * <b> Example 2: </b>
  * @code
- * // three motors on the left side of the chassis in ports 4, 5, and 6. the motor in port 5 is reversed
+ * // three motors on the left side of the chassis in ports 4, 5, and 6. The motor in port 5 is reversed
  * #define LEFT_MOTORS 4, -5, 6
  * @endcode
  * 
@@ -71,7 +72,7 @@ namespace arms {
  * 
  * <b> Example 2: </b>
  * @code
- * // three motors on the right side of the chassis in ports 1, 2, and 3. the motor in port 2 is reversed
+ * // three motors on the right side of the chassis in ports 1, 2, and 3. The motor in port 2 is reversed
  * #define RIGHT_MOTORS 1, -2, 3
  * @endcode
  * 
@@ -134,7 +135,7 @@ namespace arms {
 /*!
  * @brief IMU Port
  *
- * @param p the port the imu is plugged into. Set to 0 for disabled
+ * @param port the port the imu is plugged into. Set to 0 for disabled
  * 
  * <b>Example 1:</b>
  * @code
@@ -147,9 +148,9 @@ namespace arms {
  * #define IMU_PORT 0
  * @endcode
  *
- * @details Sets the chassis' imu to the sensor in port \a p.
+ * @details Sets the chassis' imu to the sensor in port \a port.
  */
-#define IMU_PORT 0                           // Port 0 for disabled
+#define IMU_PORT port                           // Port 0 for disabled
 
 /*!
  *
@@ -164,22 +165,42 @@ namespace arms {
  * <b>Example 1:</b>
  * @code
  * //using one parrallel encoder. IMU for heading and not worried about sidways movement
- * #define ENCODER_PORTS 1
+ * #define ENCODER_PORTS 1, 0, 0
  * @endcode
  * <b>Example 2:</b>
  * @code
- * //don't use an IMU 
- * #define IMU_PORT 0
+ * //using all three encoders. No IMU for heading, so we will need to get our heading through encoders
+ * #define ENCODER_PORTS 1, 3, 5
  * @endcode
  *
  * @details Sets up the 1-3 encoders being used on the bot. At least 1 encoder parrallel to the chassis must be used for odometry to work.
- * If either left or right encoder is blank, an IMU must also be used. An encoder perpindicular to the chassis (middle) should be used if the robot
- * is expected to be pushed sideways. Negative values reverse the direction of the encoder. The values should be valid smart ports if using the V5 
- * rotation sensor, or odd numbered adi ports if using the optical shaft encoders. This is configured at \ref ENCODER_TYPE
+ * If either left or right encoder is set to 0, an IMU must also be used. An encoder perpindicular to the chassis (middle) should be used if the robot
+ * is expected to be pushed sideways. Negative values reverse the direction of the encoder. A value of 0 disables the encoder. The values should be 
+ * valid smart ports if using the V5 rotation sensor, or odd numbered adi ports if using the optical shaft encoders. This is configured at \ref ENCODER_TYPE
+ * If all encoders are disabled, the integrated encoders in the chassis motors will be used.
  */
+#define ENCODER_PORTS left, middle, right    // Port 0 for disabled
 
-#define ENCODER_PORTS left, middle, right    // Port 0 for disabled,
-#define EXPANDER_PORT 0                      // Port 0 for disabled
+/*!
+ *
+ * @brief Encoder ADI Expander Port
+ *
+ * @param port the port the ADI expander is plugged into. 0 for disabled.
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using an ADI expander in port 1
+ * #define ADI_PORT 1
+ * @endcode
+ * <b>Example 2:</b>
+ * @code
+ * //don't use an ADI expander
+ * #define ADI_PORT 0
+ * @endcode
+ *
+ * @details Uses the expander port in port \a port for the encoder's configured at \ref ENCODER_PORTS.
+ */
+#define EXPANDER_PORT port                     // Port 0 for disabled
 
 /*!
  * @brief Encoder Type
@@ -200,35 +221,339 @@ namespace arms {
  * @details Which type of vex encoder is being used on the chassis. Using a mixture of encoder types is not currently supported.
  * This influences what the valid values for \ref ENCODER_PORTS are.
  */
-#define ENCODER_TYPE arms::odom::ENCODER_ADI // The type of encoders
+#define ENCODER_TYPE type // The type of encoders
 
 // Odometry
-#define LEFT_RIGHT_DISTANCE 0 // Distance between left and right tracking wheels
-#define MIDDLE_DISTANCE 0     // Distance from middle wheel to turning center
-#define MIDDLE_TPI 1          // Ticks per inch of middle wheel
+
+/*!
+ *
+ * @brief Left Right Distance
+ *
+ * @param dist the distance between the left and right tracking wheels
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a distance of 10 inches between the left and right tracking wheels
+ * #define LEFT_RIGHT_DISTANCE 10
+ * @endcode
+ *
+ * @details Sets the distance between the left and right tracking wheels to \a dist. Should be set to 0 if only using 1 tracker wheel
+ */
+#define LEFT_RIGHT_DISTANCE dist // Distance between left and right tracking wheels
+
+/*!
+ *
+ * @brief Middle Distance
+ *
+ * @param dist the distance between the middle tracking wheel and the turning center of the chassis
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a distance of 7 inches between the middle tracking wheel and the turning center of the chassis
+ * #define MIDDLE_DISTANCE 7
+ * @endcode
+ *
+ * @details Sets the distance between the middle tracking wheel and the turning center of the chassis to \a dist. Should be set to 0 if not using a middle tracker wheel.
+ */
+#define MIDDLE_DISTANCE dist     // Distance from middle wheel to turning center
+
+/*!
+ *
+ * @brief Middle TPI
+ *
+ * @param tpi the ticks per inch of the middle encoder wheel
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a TPI of 100
+ * #define MIDDLE_TPI 100
+ * @endcode
+ *
+* @details Sets the TPI of the middle encoder wheel.
+*/
+#define MIDDLE_TPI tpi          // Ticks per inch of middle wheel
 
 // Movement tuning
-#define SLEW_STEP 8          // Smaller number = more slew
-#define LINEAR_EXIT_ERROR 1  // default exit distance for linear movements
-#define ANGULAR_EXIT_ERROR 1 // default exit distance for angular movements
+/*!
+ *
+ * @brief Slew Step
+ *
+ * @param step how much to slew the motors by each time the motors are updated
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a slew step of 10
+ * #define SLEW_STEP 10
+ * @endcode
+ *
+ * @details Sets the slew step to \a step. A smaller value results more slew.
+ */
+#define SLEW_STEP step         // Encoder ticks to move at each step
+
+/*!
+ * 
+ * @brief Linear Exit Error
+ *
+ * @param error the error to use when exiting linear movement
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using an error of 4 units
+ * #define LINEAR_EXIT_ERROR 4
+ * @endcode
+ *
+ * @details Sets the error to use when exiting linear movement to \a error.
+ */
+#define LINEAR_EXIT_ERROR error // Error to use when exiting linear movement
+
+/*!
+ *
+ * @brief Angular Exit Error
+ *
+ * @param error the error to use when exiting angular movement
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using an error of 4 units
+ * #define ANGULAR_EXIT_ERROR 4
+ * @endcode
+ *
+ * @details Sets the error to use when exiting angular movement to \a error.
+ */
+#define ANGULAR_EXIT_ERROR error // default exit distance for angular movements
+
+/*!
+ * 
+ * @brief Linear Settle Threshold
+ *
+ * @param threshold the threshold to use when settling linear movement
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a threshold of 1 units
+ * #define LINEAR_SETTLE_THRESHOLD 1
+ * @endcode
+ *
+ * @details Sets the threshold to use when settling linear movement to \a threshold. The robot is considered settled if
+ * it does not move this many units within the duration of \ref SETTLE_TIME.
+ */
 #define SETTLE_THRESH_LINEAR .5      // amount of linear movement for settling
+
+/*!
+ * 
+ * @brief Angular settle threshold
+ *
+ * @param threshold the threshold to use when settling angular movement
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a threshold of 1 units
+ * #define ANGULAR_SETTLE_THRESHOLD 1
+ * @endcode
+ *
+ * @details Sets the threshold to use when settling angular movement to \a threshold. The robot is considered settled if
+ * it does not move this many units within the duration of \ref SETTLE_TIME.
+ */
 #define SETTLE_THRESH_ANGULAR 1      // amount of angular movement for settling
-#define SETTLE_TIME 250      // amount of time to count as settled
-#define LINEAR_KP 1
-#define LINEAR_KI 0
-#define LINEAR_KD 0
+
+/*!
+ * 
+ * @brief Settle Time
+ *
+ * @param time the time the chassis must be still to be considered settled
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a settle time of 150 milliseconds
+ * #define SETTLE_TIME 150
+ * @endcode
+ *
+ * @details Sets the time the chassis must be still to be considered settled to \a time in milliseconds.
+ * A high settle time may make movements take too long to complete, where as a low settle time may cause 
+ * the robot to exit its movement prematurely.
+ */
+#define SETTLE_TIME time      // amount of time to count as settled
+
+/*!
+ *
+ * @brief Linear kP
+ *
+ * @param kp the proportional constant for the linear motion PID controller
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a kP of 0.5
+ * #define LINEAR_KP 0.5
+ * @endcode
+ *
+ * @details Sets the proportional constant for the linear motion PID controller to \a kp.
+ */
+#define LINEAR_KP kp
+
+/*!
+ *
+ * @brief Linear kI
+ *
+ * @param ki the integral constant for the linear motion PID controller
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a ki of 0.5
+ * #define LINEAR_KI 0.5
+ * @endcode
+ *
+ * @details Sets the integral constant for the linear motion PID controller to \a ki.
+ */
+#define LINEAR_KI ki
+
+ /*!
+ *
+ * @brief Linear kD
+ *
+ * @param kd the derivative constant for the linear motion PID controller
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a kd of 0.5
+ * #define LINEAR_KD 0.5
+ * @endcode
+ *
+ * @details Sets the derivative constant for the linear motion PID controller to \a kd.
+ */
+#define LINEAR_KD kd
+
+
 #define TRACKING_KP 60		 // point tracking turning strength
-#define ANGULAR_KP 1
-#define ANGULAR_KI 0
-#define ANGULAR_KD 0
-#define MIN_ERROR 5          // Minimum distance to target before angular componenet is disabled
+
+/*!
+ *
+ * @brief Angular kP
+ *
+ * @param kp the proportional constant for the angular motion PID controller
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a kP of 0.5
+ * #define ANGULAR_KP 0.5
+ * @endcode
+ *
+ * @details Sets the proportional constant for the angular motion PID controller to \a kp.
+ */
+#define ANGULAR_KP kp
+
+ /*!
+ *
+ * @brief Angular kI
+ *
+ * @param ki the integral constant for the angular motion PID controller
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a ki of 0.5
+ * #define ANGULAR_KI 0.5
+ * @endcode
+ *
+ * @details Sets the integral constant for the angular motion PID controller to \a ki.
+ */
+#define ANGULAR_KI ki
+
+ /*!
+ *
+ * @brief Angular kD
+ *
+ * @param kd the derivative constant for the angular motion PID controller
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a kd of 0.5
+ * #define ANGULAR_KD 0.5
+ * @endcode
+ *
+ * @details Sets the derivative constant for the angular motion PID controller to \a kd.
+ */
+#define ANGULAR_KD kd
+
+/*!
+ *
+ * @brief Minimum Error
+ *
+ * @param error the minimum error for the robot to be considered at the target position
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using a minimum error of 2
+ * #define MIN_ERROR 2
+ * @endcode
+ *
+ * @details Sets the minimum error for the robot to be considered at the target position to \a error.
+ */
+#define MIN_ERROR error
 
 // Auton selector configuration constants
-#define AUTONS "Front", "Back", "Do Nothing" // Names of autonomi, up to 10
-#define HUE 0     // Color of theme from 0-359(H part of HSV)
-#define DEFAULT 1 // Default auton selected
+/*!
+ * 
+ * @brief Auton Selector Routines
+ *
+ * @param auton_names the names of your autonomous routines.
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using the autons "left", "middle", "right", and "nothing"
+ * #define AUTON_NAMES "left", "middle", "right", "nothing", ""
+ * @endcode
+ *
+ * @details Sets the auton names to run. The names should be seprated by commas. The maximum number of autons is 10.
+ * This is part of the autonomous selector configuration. More details can be found at \ref selector.h
+ */
+
+#define AUTONS auton_names // Names of autonomous routines, up to 10
+
+/*!
+ *
+ * @brief Autonomous Selctor Hue
+ *
+ * @param hue the names of your autonomous routines.
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using the autons "left", "middle", "right", and "nothing"
+ * #define AUTON_NAMES "left", "middle", "right", "nothing", ""
+ * @endcode
+ *
+ * @details Sets the auton names to run. The names should be seprated by commas. The maximum number of autons is 10.
+ * YOU MUST PUT AN EMPTY STRING AT THE END OF THE LIST.
+ * This is part of the autonomous selector configuration. More details can be found at \ref selector.h
+ */
+#define HUE hue     // Color of theme from 0-359(H part of HSV)
+
+/*!
+ *
+ * @brief Autonomous Selctor Default Routine
+ *
+ * @param index the index for the default autonomous routine to run.
+ *
+ * <b>Example 1:</b>
+ * @code
+ * //using the default auton to be "left"
+ * #define DEFAULT_AUTON 1
+ * @endcode
+ *
+ * @details Sets the default auton to run. This is part of the autonomous selector configuration. More details can be found at \ref selector.h
+ */
+#define DEFAULT index // Default auton selected
 
 // Initializer
+/*!
+ *
+ * @brief Initialize ARMS using the user defined constants
+ * <b>Example 1:</b>
+ * @code
+ * // initalize arms
+ * arms::init();
+ * @endcode
+ *
+ * @details initialize ARMS (pid, chassis, odom, etc) using the defined constants in \ref config.h
+ */
 inline void init() {
 
 	chassis::init({LEFT_MOTORS}, {RIGHT_MOTORS}, GEARSET, DISTANCE_CONSTANT,
