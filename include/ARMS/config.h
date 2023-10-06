3d6d79dc4b8a6165309986a1e3d55b511d9b5cf6
Andrew Lu <lu987@purdue.edu>
2023-10-06 15:34:50 -0400

    revert config.h, remove ARMS@3.2.0.zip

--- include/ARMS/config.h
+++ include/ARMS/config.h
@@ -9,42 +9,40 @@ namespace arms {
 #define ODOM_DEBUG 0
 
 // Negative numbers mean reversed motor
-#define LEFT_MOTORS -20, -16, -19
-#define RIGHT_MOTORS 9, 10, 11
-#define GEARSET pros::E_MOTOR_GEAR_600 // RPM of chassis motors
+#define LEFT_MOTORS 1, 2
+#define RIGHT_MOTORS -3, -4
+#define GEARSET pros::E_MOTOR_GEAR_200 // RPM of chassis motors
 
 // Ticks per inch
-#define TPI 75        // Encoder ticks per inch of forward robot movement
-#define MIDDLE_TPI 304 // Ticks per inch for the middle wheel
+#define TPI 1      			  // Encoder ticks per inch of forward robot movement
+#define MIDDLE_TPI 1          // Ticks per inch for the middle wheel
 
 // Tracking wheel distances
-#define TRACK_WIDTH                                                            \
-	3.92 // The distance between left and right wheels (or tracker wheels)
-#define MIDDLE_DISTANCE                                                        \
-	6 // Distance from middle wheel to the robot turning center
+#define TRACK_WIDTH 0 		  // The distance between left and right wheels (or tracker wheels)
+#define MIDDLE_DISTANCE 0     // Distance from middle wheel to the robot turning center
 
 // Sensors
-#define IMU_PORT 15                           // Port 0 for disabled
-#define ENCODER_PORTS 0                // Port 0 for disabled,
+#define IMU_PORT 0                           // Port 0 for disabled
+#define ENCODER_PORTS 0, 0, 0                // Port 0 for disabled,
 #define EXPANDER_PORT 0                      // Port 0 for disabled
 #define ENCODER_TYPE arms::odom::ENCODER_ADI // The type of encoders
 
 // Movement tuning
-#define SLEW_STEP 8               // Smaller number = more slew
-#define LINEAR_EXIT_ERROR 1       // default exit distance for linear movements
-#define ANGULAR_EXIT_ERROR 1      // default exit distance for angular movements
-#define SETTLE_THRESH_LINEAR .5   // amount of linear movement for settling
-#define SETTLE_THRESH_ANGULAR .25 // amount of angular movement for settling
-#define SETTLE_TIME 250           // amount of time to count as settled
-#define LINEAR_KP 3               // 8
-#define LINEAR_KI 0            // 0.01
-#define LINEAR_KD 0              // 26
-#define TRACKING_KP 60            // point tracking turning strength
-#define ANGULAR_KP 1.2 //1.2
-#define ANGULAR_KI 0 // 0.015
-#define ANGULAR_KD 5.5 //10
-#define MIN_ERROR 5 
-#define LEAD_PCT .3 // Go-to-pose lead distance ratio (0-1)
+#define SLEW_STEP 8          // Smaller number = more slew
+#define LINEAR_EXIT_ERROR 1  // default exit distance for linear movements
+#define ANGULAR_EXIT_ERROR 1 // default exit distance for angular movements
+#define SETTLE_THRESH_LINEAR .5      // amount of linear movement for settling
+#define SETTLE_THRESH_ANGULAR 1      // amount of angular movement for settling
+#define SETTLE_TIME 250      // amount of time to count as settled
+#define LINEAR_KP 1
+#define LINEAR_KI 0
+#define LINEAR_KD 0
+#define TRACKING_KP 60		 // point tracking turning strength
+#define ANGULAR_KP 1
+#define ANGULAR_KI 0
+#define ANGULAR_KD 0
+#define MIN_ERROR 5          // Minimum distance to target before angular componenet is disabled
+#define LEAD_PCT .6			 // Go-to-pose lead distance ratio (0-1)
 
 // Auton selector configuration constants
 #define AUTONS "Front", "Back", "Do Nothing" // Names of autonomi, up to 10
@@ -54,18 +52,18 @@ namespace arms {
 // Initializer
 inline void init() {
 
-	chassis::init({LEFT_MOTORS}, {RIGHT_MOTORS}, GEARSET, SLEW_STEP,
-	              LINEAR_EXIT_ERROR, ANGULAR_EXIT_ERROR, SETTLE_THRESH_LINEAR,
-	              SETTLE_THRESH_ANGULAR, SETTLE_TIME);
+	chassis::init({LEFT_MOTORS}, {RIGHT_MOTORS}, GEARSET, SLEW_STEP, LINEAR_EXIT_ERROR,
+	              ANGULAR_EXIT_ERROR, SETTLE_THRESH_LINEAR, SETTLE_THRESH_ANGULAR, SETTLE_TIME);
 
 	odom::init(ODOM_DEBUG, ENCODER_TYPE, {ENCODER_PORTS}, EXPANDER_PORT, IMU_PORT,
-	           TRACK_WIDTH, MIDDLE_DISTANCE, TPI, MIDDLE_TPI);
+	           TRACK_WIDTH, MIDDLE_DISTANCE, TPI,
+	           MIDDLE_TPI);
 
-	pid::init(LINEAR_KP, LINEAR_KI, LINEAR_KD, ANGULAR_KP, ANGULAR_KI, ANGULAR_KD,
-	          TRACKING_KP, MIN_ERROR, LEAD_PCT);
+	pid::init(LINEAR_KP, LINEAR_KI, LINEAR_KD, ANGULAR_KP, ANGULAR_KI, ANGULAR_KD, TRACKING_KP, MIN_ERROR, LEAD_PCT);
 
 	const char* b[] = {AUTONS, ""};
 	selector::init(HUE, DEFAULT, b);
+
 }
 
 } // namespace arms
